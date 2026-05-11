96a7db9204f76679ea77db3969fe8fc5162e6b12
Rodolfo Tenfen <rodolfo.tenfen@gmail.com>
2026-05-10 23:45:36 -0300

    Create project
    Default Android Studio empty Activity project.

---
+++ app/src/main/java/tenfen/rodolfo/moisesaichallenge/MainActivity.kt
@@ -0,0 +1,47 @@
+package tenfen.rodolfo.moisesaichallenge
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import tenfen.rodolfo.moisesaichallenge.ui.theme.MoisesAIChallengeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MoisesAIChallengeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MoisesAIChallengeTheme {
+        Greeting("Android")
+    }
+}
