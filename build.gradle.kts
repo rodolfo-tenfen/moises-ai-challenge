3ae5ca68ab6e0357a2805a84b8a474dda7bbfd2b
Rodolfo Tenfen <rodolfo.tenfen@gmail.com>
2026-05-11 02:09:37 -0300

    Configure Ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,11 @@ plugins {
 
     alias(libs.plugins.ktlint.gradle)
 }
+
+ktlint {
+    filter {
+        exclude { element ->
+            element.file.path.contains("generated")
+        }
+    }
+}
